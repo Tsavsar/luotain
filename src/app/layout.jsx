f2484005f1060a8e91ec7c52ad6efb1e72576db6
Tsavsar <shatermt@gmail.com>
2026-07-11 20:53:14 +0100

    mobile zoom fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,18 @@
-import "./globals.css";
+import './globals.css'
+;<meta
+  name='viewport'
+  content='width=device-width, initial-scale=1, maximum-scale=1'
+/>
 
 export const metadata = {
-  title: "Luotain",
-  description: "Shortlink and QR code generator",
-};
+  title: 'Luotain',
+  description: 'Shortlink and QR code generator',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang='en' data-theme='light'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
