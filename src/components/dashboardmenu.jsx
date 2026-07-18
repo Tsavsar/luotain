e0e116da7af245c4fd4173d5076e59c3a982edd7
Tsavsar <shatermt@gmail.com>
2026-07-18 13:37:58 +0100

    ceneter

--- src/components/dashboardmenu.jsx
+++ src/components/dashboardmenu.jsx
@@ -83,7 +83,7 @@ function OrgDropdown({ orgName, allOrgs, activeOrgId }) {
 
   return (
     <Dropdown
-      align='right'
+      align='center'
       trigger={
         <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
           <div style={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
